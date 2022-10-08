6ec04019eecef7956a3f6b8b1899944cc2902a4c
Daniel Tehrani <contact@dantehrani.com>
2022-10-08 20:44:50 +0900

    Update build_subgroupcheckG1.sh

--- scripts/subgroupcheckG1/build_subgroupcheckG1.sh
+++ scripts/subgroupcheckG1/build_subgroupcheckG1.sh
@@ -34,36 +34,25 @@ snarkjs wej "$BUILD_DIR"/witness.wtns "$BUILD_DIR"/witness.json
 
 echo "****GENERATING ZKEY 0****"
 start=`date +%s`
-NODE_OPTIONS="--max-old-space-size=56000" npx snarkjs groth16 setup "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey
+~/node/out/Release/node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc ~/snarkjs/cli.js zkey new subgroupcheckG1.r1cs ../../circuits/pot25_final.ptau subgroupcheckG1_0.zkey -v > zkey0.out
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
-echo "****GENERATING FINAL ZKEY****"
+echo "****CONTRIBUTE TO PHASE 2 CEREMONY****"
 start=`date +%s`
-NODE_OPTIONS="--max-old-space-size=56000" npx snarkjs zkey beacon "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey "$BUILD_DIR"/"$CIRCUIT_NAME".zkey 0102030405060708090a0b0c0d0e0f101112231415161718221a1b1c1d1e1f 10 -n="Final Beacon phase2"
-end=`date +%s`
-echo "DONE ($((end-start))s)"
-
-echo "****VERIFYING FINAL ZKEY****"
-start=`date +%s`
-NODE_OPTIONS="--max-old-space-size=56000" npx snarkjs zkey verify -verbose "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME".zkey
+~/node/out/Release/node ~/snarkjs/cli.js zkey contribute -verbose subgroupcheckG1_0.zkey subgroupcheckG1.zkey -n="First phase2 contribution" -e="some random text 5555" > contribute.out
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****EXPORTING VKEY****"
 start=`date +%s`
-npx snarkjs zkey export verificationkey "$BUILD_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/vkey.json
+~/node/out/Release/node ~/snarkjs/cli.js zkey export verificationkey subgroupcheckG1.zkey vkey.json -v
 end=`date +%s`
 echo "DONE ($((end-start))s)"
 
 echo "****GENERATING PROOF FOR SAMPLE INPUT****"
 start=`date +%s`
-rapidsnark "$BUILD_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/witness.wtns "$BUILD_DIR"/proof.json "$BUILD_DIR"/public.json
-end=`date +%s`
-echo "DONE ($((end-start))s)"
-
-echo "****VERIFYING PROOF FOR SAMPLE INPUT****"
-start=`date +%s`
-npx snarkjs groth16 verify "$BUILD_DIR"/vkey.json "$BUILD_DIR"/public.json "$BUILD_DIR"/proof.json
+~/rapidsnark/build/prover subgroupcheckG1.zkey witness.wtns proof.json public.json > proof.out
+#~/node/out/Release/node ~/snarkjs/cli.js subgroupcheckG1.zkey witness.wtns proof.json public.json > proof.out
 end=`date +%s`
 echo "DONE ($((end-start))s)"
